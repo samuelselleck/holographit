853c0128ff3b3fc3737ed3e0f06002d3e246efdf
Benjamin Gordon <ben@Benjamins-MacBook-Pro.local>
2023-02-24 17:24:14 -0600

    loads obj from command line arguments

--- holoscribe/src/main.rs
+++ holoscribe/src/main.rs
@@ -3,6 +3,29 @@ mod scriber;
 //cli accepts obj file and svg location (and optional parameters)
 mod cli;
 
-fn main() {
-    println!("Hello, world!");
+use obj::Obj;
+use std::env;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // TODO: use Clap if this gets unwieldy in the future
+    let args: Vec<String> = env::args().collect();
+    let input_model_file_path = args[1].clone();
+
+    let user_defined_model = obj_from_file(input_model_file_path).unwrap();
+    let verts = user_defined_model.data.position;
+    dbg!(verts);
+
+    Ok(())
+}
+
+// load an obj struct from a file
+fn obj_from_file(file_path: String) -> Result<Obj, Box<dyn Error>> {
+    let user_defined_model: Obj = Obj::load(file_path)?;
+    return Ok(user_defined_model);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::obj_from_file;
 }
