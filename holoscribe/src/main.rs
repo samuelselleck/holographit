756ffb18e8c67e79750575c6478a48e89337e5f6
Samuel Selleck <sellecksamuel@gmail.com>
2023-02-24 23:21:09 +0100

    initial commit

---
+++ holoscribe/src/main.rs
@@ -0,0 +1,8 @@
+//responsible for transforming point clouds/obj files to a holographic svg
+mod scriber;
+//cli accepts obj file and svg location (and optional parameters)
+mod cli;
+
+fn main() {
+    println!("Hello, world!");
+}
