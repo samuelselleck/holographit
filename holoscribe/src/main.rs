c95cb6e131b6c60775e9103ef32d9275faaad721
Samuel Selleck <sellecksamuel@gmail.com>
2023-02-27 21:12:45 +0100

    scriber + scribestrat

--- holoscribe/src/main.rs
+++ holoscribe/src/main.rs
@@ -14,8 +14,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let user_defined_model = obj_from_file(input_model_file_path).unwrap();
     let verts = user_defined_model.data.position;
-    dbg!(verts);
 
+    let arc_strat = scriber::DebugScriber {
+        plane_start: -1.0,
+        plane_end: 1.0,
+        min_size: 0.1,
+        max_size: 0.4,
+    };
+    let scriber = scriber::Scriber::new(arc_strat);
+    let svg = scriber.scribe(&verts);
+
+    svg::save("test.svg", &svg).expect("failed to save");
     Ok(())
 }
 
---
+++ holoscribe/src/scriber/mod.rs
@@ -0,0 +1,106 @@
+use svg::node::element::path::Data;
+use svg::node::element::Path;
+use svg::Document;
+
+pub struct DebugScriber {
+    pub plane_start: Num,
+    pub plane_end: Num,
+    pub min_size: Num,
+    pub max_size: Num,
+}
+
+impl DebugScriber {
+    fn map_range(&self, z: Num) -> Num {
+        self.min_size
+            + (z - self.plane_start) * (self.max_size - self.min_size)
+                / (self.plane_end - self.plane_start)
+    }
+}
+impl HoloPointStrategy for DebugScriber {
+    fn scribe_point(&self, data: Data, point: &[Num; 3]) -> Data {
+        let &[x, y, z] = point;
+        let z = self.map_range(z);
+        data.move_to((x, y))
+            .move_by((0, -z))
+            .line_by((-z, z))
+            .line_by((z, z))
+            .line_by((z, -z))
+            .line_by((-z, -z))
+    }
+}
+
+//This could later on use different strategies to visualize a point
+//ie. perfect, arc, something else interesting
+pub trait HoloPointStrategy {
+    fn scribe_point(&self, data: Data, point: &[Num; 3]) -> Data;
+}
+
+pub struct Scriber {
+    //Ammount to increase the document view in x/y beyond furthest away points
+    //This effects if some arcs are drawn outisde the work area
+    margin: Num,
+    stroke: &'static str,
+    stroke_width: Num,
+    point_scribing_strategy: Box<dyn HoloPointStrategy>,
+}
+
+type Num = f32;
+
+impl Scriber {
+    pub fn new(point_scribing_strategy: impl HoloPointStrategy + 'static) -> Self {
+        Self {
+            margin: 1.0,
+            stroke: "black",
+            stroke_width: 0.05,
+            point_scribing_strategy: Box::new(point_scribing_strategy),
+        }
+    }
+    /*
+    Assumtions:
+    - x points right
+    - y points up
+    - z is positive out of the screen
+    */
+    pub fn scribe(&self, points: &Vec<[Num; 3]>) -> svg::Document {
+        let ((x_min, x_max), (y_min, y_max), _) = self.bounds(points);
+        let data = points.iter().fold(Data::new(), |d, p| {
+            self.point_scribing_strategy.scribe_point(d, p)
+        });
+        let data = data //add bounding box for now
+            .move_to((x_min, y_min))
+            .line_to((x_min, y_max))
+            .line_to((x_max, y_max))
+            .line_to((x_max, y_min))
+            .line_to((x_min, y_min));
+        let path = Path::new()
+            .set("fill", "none")
+            .set("stroke", self.stroke)
+            .set("stroke-width", self.stroke_width)
+            .set("d", data);
+        Document::new()
+            .set("viewBox", (x_min, y_min, x_max - x_min, y_max - y_min))
+            .add(path)
+    }
+
+    //x, y and z point bounds + margins
+    fn bounds(&self, points: &Vec<[Num; 3]>) -> ((Num, Num), (Num, Num), (Num, Num)) {
+        let (mut x_min, mut y_min, mut z_min) = (Num::MAX, Num::MAX, Num::MAX);
+        let (mut x_max, mut y_max, mut z_max) = (Num::MIN, Num::MIN, Num::MIN);
+
+        for &[x, y, z] in points {
+            x_min = x_min.min(x);
+            x_max = x_max.max(x);
+            y_min = y_min.min(y);
+            y_max = y_max.max(y);
+            z_min = z_min.min(z);
+            z_max = z_max.max(z);
+        }
+
+        let m = self.margin;
+        (
+            (x_min - m, x_max + m),
+            (y_min - m, y_max + m),
+            (z_min - m, z_max + m),
+        )
+    }
+}
