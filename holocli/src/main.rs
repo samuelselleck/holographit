7cb17d29882c94331b5d842166d4c9d0e46cb94e
Sara Hartse <sara.hartse@gmail.com>
2023-03-07 16:15:36 -0800

    Rework size scaling for hologram scribing

--- holocli/src/main.rs
+++ holocli/src/main.rs
@@ -5,17 +5,20 @@ mod cli;
 
 use std::error::Error;
 
-use holoscribe::{model::ObjInterpolator, scriber};
 use clap::Parser;
 use cli::Args;
+use holoscribe::{model::ObjInterpolator, scriber};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let user_defined_model = ObjInterpolator::from_file(args.input).unwrap();
     let interpolated_points = user_defined_model.interpolate_edges(args.stroke_density);
 
-    let circle_strat = scriber::CircleScriber {};
-    let scriber = scriber::Scriber::new(circle_strat, (args.canvas_size.width, args.canvas_size.height));
+    let circle_strat = scriber::CircleScriber::new();
+    let scriber = scriber::Scriber::new(
+        circle_strat,
+        (args.canvas_size.width, args.canvas_size.height),
+    );
 
     let svg = scriber.scribe(&interpolated_points);
 
